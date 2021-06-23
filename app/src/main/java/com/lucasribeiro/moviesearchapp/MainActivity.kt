8ae112f29497b7ba6dbf1dbd5f0eda1bd9163981
Lucas Ribeiro <lrpessamilio@gmail.com>
2021-06-23 16:37:57 -0300

    Built interface for the data, but keeps crashing

--- app/src/main/java/com/lucasribeiro/moviesearchapp/MainActivity.kt
+++ app/src/main/java/com/lucasribeiro/moviesearchapp/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.lucasribeiro.moviesearchapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Picasso
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +21,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun requestMovie(view: View){
-        val resultsView = findViewById<TextView>(R.id.ResultsText)
+    @SuppressLint("SetTextI18n")
+    fun requestMovie(view: View) {
+        val resultsTitleText = findViewById<TextView>(R.id.resultsTitleText)
+        val resultsYearText = findViewById<TextView>(R.id.resultsYearText)
+        val resultsPosterImg = findViewById<ImageView>(R.id.resultsPosterImg)
         val queue = Volley.newRequestQueue(this)
         val searchTerm = findViewById<EditText>(R.id.MovieNameForm).text
         val url = "https://www.omdbapi.com/?apikey=${BuildConfig.OMBD_KEY}&t=${searchTerm}"
@@ -27,8 +33,11 @@ class MainActivity : AppCompatActivity() {
 
         val stringRequest = JsonObjectRequest(Request.Method.GET, url, null,
             { res ->
-                resultsView.text = "The response was ${res["Title"]}" },
-            { resultsView.text = "Error, Request failed" })
+                resultsTitleText.text = "${res["Title"]}"
+                resultsYearText.text = "${res["Year"]}"
+                Picasso.get().load("${res[" Poster "]}").into(resultsPosterImg)
+            },
+            { resultsTitleText.text = "Error, Request failed" })
         queue.add(stringRequest)
     }
 
