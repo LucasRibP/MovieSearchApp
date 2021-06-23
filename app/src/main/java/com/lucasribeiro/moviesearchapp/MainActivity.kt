f4096e279e9e185d0effaf6c71afa80d2d9d7103
Lucas Ribeiro <lrpessamilio@gmail.com>
2021-06-22 21:12:37 -0300

    Started integrating the app to the API

--- app/src/main/java/com/lucasribeiro/moviesearchapp/MainActivity.kt
+++ app/src/main/java/com/lucasribeiro/moviesearchapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.lucasribeiro.moviesearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    fun requestMovie(view: View){
+        val resultsView = findViewById<TextView>(R.id.ResultsText)
+        val queue = Volley.newRequestQueue(this)
+        val searchTerm = findViewById<EditText>(R.id.MovieNameForm).text
+        val url = "http://www.omdbapi.com/?apikey=${BuildConfig.OMBD_KEY}&t=${searchTerm}"
+
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            { res ->
+                resultsView.text = "The response was ${res.substring(0, 500)}" },
+            { resultsView.text = "Error, Request failed" })
+        queue.add(stringRequest)
+    }
+
+
 }
